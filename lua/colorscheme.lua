4669e64c11ce9dc19d109b6a5bdc73f5da55907d
Hector Bjernersjö <hector@bjernersjo.se>
2024-08-22 00:00:53 +0200

    theme switcher start

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -7,7 +7,26 @@ return {
 		"folke/tokyonight.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			vim.cmd.colorscheme("tokyonight-night")
+			-- Set default colorscheme
+			local colorscheme = "tokyonight-night"
+
+			-- Path to the colorscheme file
+			local colorscheme_file = vim.fn.stdpath("config") .. "/colorscheme.txt"
+
+			-- Check if the file exists and read the colorscheme from it
+			if vim.fn.filereadable(colorscheme_file) == 1 then
+				local file = io.open(colorscheme_file, "r")
+				if file then
+					local content = file:read("*all"):gsub("%s+", "")
+					if content and #content > 0 then
+						colorscheme = content
+					end
+					file:close()
+				end
+			end
+
+			-- Apply the colorscheme
+			vim.cmd.colorscheme(colorscheme)
 			vim.cmd.hi("Comment gui=none")
 		end,
 	},
